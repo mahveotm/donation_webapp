3f78cc725a2057428336e5af944b51fc7130c69e
mahveotm <mahveotm@gmail.com>
2021-05-20 14:39:34 +0100

    /

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config
-
+from datetime import timedelta
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -10,6 +10,7 @@ bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    app.permanent_session_lifetime = timedelta(minutes=5)
     config[config_name].init_app(app)
     
     bootstrap.init_app(app)
